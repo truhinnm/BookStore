91cb2f16648c96ba7feb436bb8c54938d6c1236f
Nik Trush <nick_trush@mail.ru>
2021-02-20 23:02:38 +0300

    Homework 20.02.2021

---
+++ HW_2_Sem/ConsoleAppTut/ConsoleAppTut/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTut
+{
+    public class Startup
+    {
+        public void ConfigurationServices(IServiceCollection service)
+        {
+
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context => 
+                {
+                    await context.Response.WriteAsync("DDDDDDDDD");
+                });
+            });
+        }
+    }
+}
